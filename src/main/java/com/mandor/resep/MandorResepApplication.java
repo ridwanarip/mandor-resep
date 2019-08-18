166382a15f12ebe4a335d36c539151114a4abf4c
ridwan arip <ridwanarip@yahoo.com>
2019-08-18 14:30:20 +0700

    Initial source

---
+++ src/main/java/com/mandor/resep/MandorResepApplication.java
@@ -0,0 +1,13 @@
+package com.mandor.resep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MandorResepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MandorResepApplication.class, args);
+	}
+
+}
