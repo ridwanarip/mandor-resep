24bf74e417374325ed18e25f1fbd40fd8613cb98
ridwan arip <ridwanarip@yahoo.com>
2019-08-21 11:16:31 +0700

    edit

--- src/main/java/com/mandor/resep/controller/IndexController.java
+++ src/main/java/com/mandor/resep/controller/IndexController.java
@@ -1,14 +1,17 @@
 package com.mandor.resep.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.mandor.resep.model.Product;
 import com.mandor.resep.service.ProductService;
@@ -44,5 +47,14 @@ public class IndexController {
 	public String saveProduct(@ModelAttribute("product") Product product) {
 		service.save(product);
 		return "redirect:/";
+	}
+	
+	@GetMapping("/edit/{id}")
+	public ModelAndView showEditProdcutEditPage(@PathVariable(name = "id") long id) {
+		ModelAndView mav = new ModelAndView("edit_product");
+		Optional<Product> product = service.getProduct(id);
+		mav.addObject("product", product);
+		
+		return mav;
 	}
 }
